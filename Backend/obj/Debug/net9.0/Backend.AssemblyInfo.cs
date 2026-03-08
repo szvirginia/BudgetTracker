17c22843cb266f93ad324d4ce48414f7804e85e5
Szalai Virgínia <szvirgi@gmail.com>
2026-03-08 17:04:39 +0100

    Updated binary files and assembly information for backend

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3b76a44d3ca93acb5d3088f3263c13dd5577a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b26cd5d21d5b98bcf7fb1d92ab7a5fb0e8fafd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
